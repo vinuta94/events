8093708519f2fcd44ef1157d124220b012b63cc0
vinuta94 <vinutakorishettar@gmail.com>
2017-05-18 14:44:58 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 
 <?php
-$target_dir = "/var/www/html/events/";
+$target_dir = "/var/www/html/events/uploads";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
